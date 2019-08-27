cb2262e89d11f676e5b561e50f535d25dca98811
Earl Tankard <earl.tankard@bison.howard.edu>
2019-08-27 20:41:10 +0000

    Assumptions && Requirements v1.0

--- marginMaker.py
+++ marginMaker.py
@@ -1,8 +1,19 @@
 '''
 Assumptions
-  * "DAT1.TXT" is properly formatted grammatically
+  * "DAT1.TXT" is properly formatted (grammatically)
   * The user types in the two integers (the left and right margins)
+  * 1 inch = 12 characters (X) --> one inch contains 12 chars
+  * Line is max of 80 chars total
   
 Requirements
-  * Create a new textfile ("DAT1_output.TXT")
+  * Get margins from user
+  * Read text from "DAT1.TXT"
+  * Format text in accordance to user-set margins, spacing, etc.
+  * Display edited text
+  * Create a new textfile with the edited text ("DAT1_output.TXT")
+  
+Questions
+  * What are "trailing blanks"?
+  
+
 '''
