00b402d45dc195397bcf088ff2527be60e93a2ef
Earl Tankard <earl.tankard@bison.howard.edu>
2019-08-27 18:55:53 +0000

    Setting up assumptions and requirements

---
+++ marginMaker.py
@@ -0,0 +1,8 @@
+'''
+Assumptions
+  * "DAT1.TXT" is properly formatted grammatically
+  * The user types in the two integers (the left and right margins)
+  
+Requirements
+  * Create a new textfile ("DAT1_output.TXT")
+'''
